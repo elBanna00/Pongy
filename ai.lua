02e64e111dcc1c80b7076753964b82852e9c9af2
moaaz <moaazibrahim3@gmail.com>
2024-03-24 19:28:19 +0200

    Add AI Model and Collision

---
+++ ai.lua
@@ -0,0 +1,44 @@
+AI = {}
+
+
+function AI:load()
+  self.width = 20
+  self.height = 100
+  self.x = love.graphics.getWidth() - self.width  - 50
+  self.y = love.graphics.getHeight() /2
+  self.yVel = 0
+  self.speed = 500
+--A timer that serves as a delay untill it reaches the rate Value then excute the required action (Check the update Function)
+  self.timer = 0
+  self.rate = 0.5
+end
+
+
+function AI:update(dt)
+  self:move(dt)
+  self.timer = self.timer + dt
+  if self.timer > self.rate then
+    self.timer = 0
+    self:followTarget()
+  end
+end
+
+function AI:move(dt)
+
+  self.y = self.y + self.yVel * dt
+
+end
+
+function AI:followTarget()
+  if Ball.y + Ball.height < self.y then
+    self.yVel = -self.speed
+  elseif Ball.y > self.y + self.height then
+    self.yVel = self.speed
+  else
+    self.yVel = 0
+  end
+end
+
+function AI:draw()
+  love.graphics.rectangle("fill",self.x ,self.y , self.width, self.height)
+end
